ff51326b6511f8d84d7bf831fd609996702a3b94
mhemberger <mikehemberger@gmail.com>
2022-01-21 22:07:40 +0100

    Upload explore nb and initial app.py

--- app.py
+++ app.py
@@ -1,3 +1,27 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-st.write("HELLO ")
+# Later download the csv daily anew > link to github
+df = pd.read_csv("./corona_ts.csv", parse_dates=True)
+
+df = df.rename(columns={
+    "Country/Region" : "country",
+    "Province/State" : "state"})
+
+st.title("Corona statistics dashboard")
+
+ccountry = st.select_slider("Pick a country", options=df.country.tolist())
+# have to remove the "states" here and sort alpabetically
+
+
+ts_min = 4  # measurements begin
+t = df.columns.tolist()[ts_min:]
+
+dfg = df.groupby(by="country").sum() # can go upwards
+dfg = dfg.reset_index()
+idx = dfg[dfg.country == ccountry].index.tolist()
+y = np.squeeze(dfg.loc[idx, t].values)
+
+st.line_chart(y)
