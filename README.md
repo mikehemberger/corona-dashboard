b35dbdd55873aefa85658a29b75b58a795914e52
mhemberger <mikehemberger@gmail.com>
2022-01-28 00:33:33 +0100

    Update nb, app and README

--- README.md
+++ README.md
@@ -4,8 +4,14 @@ The notebook [explore-data.ipynb]() visualizes various corona statistics.
 The interesting results from this exploration are then integrated into the streamlit app. 
 
 ```
+pip3 install millify
+```
+
+```
 streamlit run app.py
 ```
+
+Reading the .CSV files from URL requires the "RAW" data link eg: `https://raw.githubusercontent.com/...`
 
 ### Streamlit info
 deploy app: https://share.streamlit.io/signup (Once deployed, reachable: https://share.streamlit.io/mikehemberger/corona-dashboard/main/app.py
@@ -18,3 +24,6 @@ python api: https://docs.streamlit.io/library/api-reference
 - Big diverse data: https://www.ecdc.europa.eu/en/covid-19/data
 - RKI dashboard: https://experience.arcgis.com/experience/478220a4c454480e823b17327b2bf1d4/page/Bundesl%C3%A4nder/
 
+Omicron timeline - https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1XjAYFLM5Rvh9l9ySqMbb_NVkWRpKsSQBDmwytmPdLUg&font=Default&lang=en&initial_zoom=2&height=650
+
+B.1.1.529 - Omicron
--- app.py
+++ app.py
@@ -2,33 +2,38 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from millify import millify
+
+plt.rc('font', size=15)
 plt.style.use('dark_background')
 
-st.title("Corona statistics dashboard")
-
-
 # Summed data as metrics
-dfc = pd.read_csv("./data/corona_confirmed_ts_22-01-2022.csv", parse_dates=True)  # infection data
-dfd = pd.read_csv("./data/corona_deaths_ts_22-01-2022.csv", parse_dates=True)  # deaths
+url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+dfc = pd.read_csv(url, parse_dates=True)  # infection data
+
+url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+dfd = pd.read_csv(url, parse_dates=True)
+
 t = dfc.columns.tolist()[4:]
 
 dfc = dfc.groupby(by="Country/Region").sum()
 dfd = dfd.groupby(by="Country/Region").sum()
 
-denom = 3
-conf = np.round(dfc.loc[:, t[-1]].sum() / 1e6, denom)
-dconf = np.round((dfc.loc[:, t[-1]].sum() - dfc.loc[:, t[-2]].sum()) / 1e6, denom)
+conf = dfc.loc[:, t[-1]].sum()
+dconf = dfc.loc[:, t[-1]].sum() - dfc.loc[:, t[-2]].sum()
 
-deaths = np.round(dfd.loc[:, t[-1]].sum() / 1e6, denom)
-ddeaths = np.round((dfd.loc[:, t[-1]].sum() - dfd.loc[:, t[-2]].sum()) / 1e6, denom)
+deaths = dfd.loc[:, t[-1]].sum()
+ddeaths = dfd.loc[:, t[-1]].sum() - dfd.loc[:, t[-2]].sum()
+
+# Streamlit start
+st.title("Corona statistics dashboard " + t[-1])
 
 # Define columns
 met1, met2, met3 = st.columns(3)
 
-met1.metric(label="Confirmed Cases in millions", value=conf, delta=dconf)
-met2.metric(label="Death Cases in millions", value=deaths, delta=ddeaths)
+met1.metric(label="Confirmed Cases", value=millify(conf, precision=2), delta=millify(dconf, precision=2))
+met2.metric(label="Death Cases", value=millify(deaths, precision=2), delta=millify(ddeaths, precision=2))
 # add the vaccination numbers here as met3
-
 
 # Later download the csv daily anew > link to github
 df = pd.read_csv("./data/corona_confirmed_ts_22-01-2022.csv", parse_dates=True)
@@ -37,28 +42,39 @@ df = df.rename(columns={
     "Country/Region" : "country",
     "Province/State" : "state"})
 
-ccountry = st.select_slider("Pick a country", options=df.country.tolist())
-# have to remove the "states" here and sort alpabetically
-
-
 ts_min = 4  # measurements begin
 t = df.columns.tolist()[ts_min:]
+#tt = len(df.columns.tolist()) - ts_min
 
 dfg = df.groupby(by="country").sum() # can go upwards
 dfg = dfg.reset_index()
+
+ccountry = st.sidebar.select_slider("Select a country", options=dfg.country.tolist())
+# have to remove the "states" here and sort alpabetically
 idx = dfg[dfg.country == ccountry].index.tolist()
+
+# Accumulated infections
 y = np.squeeze(dfg.loc[idx, t].values) / 1e6
+
+# Rolling average over daily new infected
+prev = st.sidebar.select_slider("Show last days", options=range(len(t)))
+t_prev = t[-prev:]
+
+#y2 = np.squeeze(dfg.loc[idx, t].diff(axis=1).transpose().rolling(window=7).mean())
+y2 = np.squeeze(dfg.loc[idx, t_prev].diff(axis=1).transpose().rolling(window=7).mean())
+y2 = np.array(y2)
+#y2 = dfg.loc[idx, t].diff(axis=1).T.rolling(window=7).mean()
 
 # Def figure columns
 col1, col2 = st.columns(2)
 
-fig = plt.figure()
+fig = plt.figure(figsize=(4,4))
 plt.plot(y)
-plt.ylabel("Something")
+plt.ylabel("Accumulated infections (Mio.)")
 col1.pyplot(fig)
 
-fig = plt.figure()
-plt.plot(np.diff(y))  # plot this in weeks
-plt.ylabel("Something")
+fig = plt.figure(figsize=(4,4))
+plt.plot(y2)  # plot this in weeks
+plt.ylabel("Daily infections")
 col2.pyplot(fig)
 #st.line_chart(y)
